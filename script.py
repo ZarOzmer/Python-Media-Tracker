02152deb02037164a8372aa5c96476dcecf0617a
Zaria Ozmer <zariaozmer@gmail.com>
2023-11-20 15:01:14 -0700

    Media removal functonality and commants

--- script.py
+++ script.py
@@ -1,4 +1,3 @@
-import os.path
 import random
 import csv
 import os
@@ -8,6 +7,7 @@ if not os.path.isfile('mediaTracker.csv'):
         mediaTracker.write("Type|Name|Location\n")
         
 def clear():
+    #for windowsOS
     if os.name == 'nt':
         _ = os.system('cls')
     # For macOS and Linux
@@ -21,25 +21,67 @@ def addMedia():
     with open("mediaTracker.csv", 'a') as mediaTracker:
         mediaTracker.write("{}|{}|{}\n".format(mtype, mname, mfind))
 
-def getMeda():
+def getMedia():
     with open('MediaTracker.csv', newline='') as file:
         mediaCSV = csv.reader(file, delimiter = "|")
         next(mediaCSV)
         mediaList = []
         stype = input("what type of media do you want to consume? for any just hit enter \n").capitalize()
-        
+    #creates a list of all the media of a certen type or if no type given a list of all the media the program tracks
         for row in mediaCSV:
             if stype in row[0]:
                 mediaList.append(row)
+    #selects a random peice of media from that list
     if len(mediaList) > 1:
         media = mediaList[random.randint(0, (len(mediaList)) -1 )]
+    #unless the list is to short
     elif len(mediaList) == 1: media = mediaList[0]
     else:
         print("You Have no media of that type saved")
         return()
     print("your media is: {name}, it is a {type} that can be found at {location}.\n".format(name = media[1], type = media[0], location = media[2]))
 
-    
+def removeMedia():
+    mtype = str(input("What kind of media are you removing?\n").capitalize())
+    medialist = []
+    with open('MediaTracker.csv', newline='') as file:
+        mediaCSV = csv.reader(file, delimiter = "|")
+        next(mediaCSV)
+    #generates a list of all the media of the given type
+        for row in mediaCSV:
+                print(row[0])
+                print(mtype)
+                print(mtype in row[0])
+                print("")
+                if mtype in row[0]:
+                    medialist.append(row)
+    #prunes the list of media who's names dont contane a search string
+    mserch = input("search criteria for the name of the media you wish to remove\n")
+    for row in medialist:
+        if not mserch.lower() in str(row[1]).lower():
+            medialist.remove(row)
+    if not len(medialist) > 0:
+        print("No media matches your serch criteria")
+        return
+    for x in range(len(medialist)):
+        print(str(x) + ": " + str(medialist[x]))
+    input3 = input("enter the number of the media you wish to remove\n")
+    #what's to be removed 
+    badMedia = medialist[int(input3)]
+    #generates a list of everthing not removed
+    goodMedia = []
+    with open('MediaTracker.csv', ) as file:
+        mediaCSV = csv.reader(file, delimiter = "|")
+        next(mediaCSV)
+        for row in mediaCSV:
+            if not row == badMedia:
+                goodMedia.append(row)
+    #overwrites the file with the list of not removed things after adding the header
+    with open('MediaTracker.csv', 'w') as mediaCSV:
+        mediaCSV.write("Type|Name|Location\n")
+        for line in goodMedia:
+            mediaCSV.write("{}|{}|{}\n".format(line[0],line[1],line[2]))
+
 
 
 
@@ -62,7 +104,10 @@ while True:
     if userIn == '-A':
         addMedia()
     elif userIn == '-B':
-        getMeda()
+        getMedia()
     elif userIn == '-E':
         break
+    elif userIn == '-R':
+        removeMedia()
     temp =input("press enter when you're done with what's currently on screen")
+
